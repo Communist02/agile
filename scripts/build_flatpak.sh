ee8c7d6188d0c94cce266921776885461895ac09
Denis Mazur <kilerden2002@gmail.com>
2025-07-04 13:53:31 +1000

    Update

--- scripts/build_flatpak.sh
+++ scripts/build_flatpak.sh
@@ -2,3 +2,6 @@ cd ../
 flatpak-builder build-dir flatpak.yaml --force-clean
 flatpak build-export rclone-explorer-repo build-dir
 flatpak build-bundle rclone-explorer-repo RcloneExplorer.flatpak com.mazur.RcloneExplorer
+flatpak uninstall -y com.mazur.RcloneExplorer
+flatpak install -y --bundle --user RcloneExplorer.flatpak
+flatpak run com.mazur.RcloneExplorer
